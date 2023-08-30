c6f325b60507b315a16ab8e61b0dac0ac41b592f
photobaric <129235643+photobaric@users.noreply.github.com>
2023-08-30 17:03:18 -0400

    clean up

--- extra/circle-rasterizer.c
+++ extra/circle-rasterizer.c
@@ -13,17 +13,17 @@ int main(void)
     int Cx = 32;
     int Cy = 32;
     int R = 20;
-    
+
     // NOTE(casey): Loop that draws the circle
     {
         int R2 = R+R;
-        
+
         int X = R;
         int Y = 0;
         int dY = -2;
         int dX = R2+R2 - 4;
         int D = R2 - 1;
-        
+
         while(Y <= X)
         {
             Bitmap[Cy - Y][Cx - X] = 1;
@@ -34,11 +34,11 @@ int main(void)
             Bitmap[Cy - X][Cx + Y] = 1;
             Bitmap[Cy + X][Cx - Y] = 1;
             Bitmap[Cy + X][Cx + Y] = 1;
-            
+
             D += dY;
             dY -= 4;
             ++Y;
-        
+
 #if 0
             // NOTE(casey): Branching version
             if(D < 0)
@@ -56,7 +56,7 @@ int main(void)
 #endif
         }
     }
-    
+
     // NOTE(casey): Output the bitmap using roughly square elements
     printf("P1\n");
     printf("64 64\n");
